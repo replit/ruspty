2aeef953e20d63a74c9deddf9195b4a5804ab917
lhchavez <lhchavez@lhchavez.com>
2024-05-03 10:04:26 -0700

    [ci] Fix CI (#7)
    #4 added a lot of good CI improvements. Let's land them ASAP!
    
    This change takes #4 and only keeps the changes needed to fix CI and
    local Linux development.
    
    ---------
    
    Co-authored-by: Szymon Kaliski <hi@szymonkaliski.com>

--- flake.nix
+++ flake.nix
@@ -20,5 +20,7 @@
   {
     devShells.aarch64-darwin.default = mkDevShell "aarch64-darwin";
     devShells.x86_64-darwin.default = mkDevShell "x86_64-darwin";
+    devShells.aarch64-linux.default = mkDevShell "aarch64-linux";
+    devShells.x86_64-linux.default = mkDevShell "x86_64-linux";
   };
 }
