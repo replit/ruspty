e181c709565bd6dd212bcf533fb79e82a8fa5bd1
lhchavez <lhchavez@lhchavez.com>
2024-07-09 10:02:24 -0700

    3.1.0 Add functions to get/set the close-on-exec flag (#34)
    pid2 needs to set the close-on-exec flag on a certain file descriptor,
    but node doesn't have an API to achieve this. The only other node module
    that provides the `fcntl(2)` binding doesn't play well with esbuild, so
    we need to recruit the help of our friend here.
    
    This change adds two functions to get/set the close-on-exec flag on a
    file descriptor.

--- index.js
+++ index.js
@@ -310,6 +310,8 @@ if (!nativeBinding) {
   throw new Error(`Failed to load native binding`)
 }
 
-const { Pty } = nativeBinding
+const { Pty, setCloseOnExec, getCloseOnExec } = nativeBinding
 
 module.exports.Pty = Pty
+module.exports.setCloseOnExec = setCloseOnExec
+module.exports.getCloseOnExec = getCloseOnExec
