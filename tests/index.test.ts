e908971c3259761225465b1b30c0fd66d9f03f35
Jacky Zhao <j.zhao2k19@gmail.com>
2024-10-25 15:22:38 -0700

    run all the tests now

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -283,7 +283,7 @@ describe(
       expect(onExit).toHaveBeenCalledWith(null, -1);
     });
 
-    test.only(
+    test(
       'ordering is correct',
       async () => {
         console.log('-- start --')
